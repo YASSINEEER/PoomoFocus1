d60c2b260a15916de76218b9737e06879578183b
YASSINEEER <89854601+YASSINEEER@users.noreply.github.com>
2022-11-09 03:23:35 +0800

    Add files via upload

---
+++ main/java/com/TodoFocus/yassine/LongBreakFragments.java
@@ -0,0 +1,19 @@
+package com.TodoFocus.yassine;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LongBreakFragments extends Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_long_break_fragments, container, false);
+    }
+}
